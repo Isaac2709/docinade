b6af61a8b1254c78d2440202fe80570410a8fc07
Isaac2709 <Isaac2709@hotmail.com>
2015-04-15 00:13:17 -0600

    Add the Pais seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder {
 
@@ -14,9 +15,9 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		$this->call('UserTableSeeder');
+		$this->call('PaisTableSeeder');
 
-		$this->command->info('User table seeded!');
+		$this->command->info('Pais table seeded!');
 	}
 
 }
