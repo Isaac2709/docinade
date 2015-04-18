6a641f192af2cb26340f07c184ede742374751d2
Isaac2709 <Isaac2709@hotmail.com>
2015-04-18 01:39:54 -0600

    Added the Pais model and updated the User, Formulario and InformacionAspirante models. Also add the combobox and calendar resources to worked in that

--- app/Formulario.php
+++ app/Formulario.php
@@ -4,9 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Formulario extends Model {
 
-	protected $table = 'ASP_Aspirante';
+	protected $table = 'GEN_Info_Personal';
 
-	protected $primaryKey = 'Asp_ID';
+	protected $primaryKey = 'IPe_ID';
 
 	/**
 	* Set the variables timestamps to false
@@ -15,14 +15,25 @@ class Formulario extends Model {
 	*/
 	public $timestamps = false;
 
-	// protected $fillable = [ 'Asp_ID', 'Asp_Fecha_Envio', 'Asp_Fotografia', 'Asp_Pasaporte_Adj',
-	// 						'Asp_ID_InfoPer', 'Asp_Lugar_Nac', 'Asp_ID_Nac', 'Asp_ID_Enfasis',
-	// 						'Asp_ID_Dir_Actual', 'Asp_ID_Area_Interes', 'Asp_Acceso_Biblioteca',
-	// 						'Asp_Acceso_Proc_DatoS', 'Asp_Acceso_Windows', 'Asp_Acceso_Email',
-	// 						'Asp_Utilizacion_Progra_Comp', 'Asp_Conoc_Educacion_Dist',
-	// 						'ID_Prop_Tesis'];
+
+
+	// protected $fillable = [ 'IPe_ID', 'IPe_Nombre', 'IPe_Apellido', 'IPe_Genero', 'IPe_Pasaporte',
+	// 						'IPe_Fecha_Nac', 'IPe_ID_PaisRes', 'IPe_Telefono', 'IPe_Fax'];
 
 	public function aspirante(){
 		return $this->belongsTo('User', 'GEN_ID_Usuario');
 	}
+
+	public function informacion_aspirante(){
+		return $this->hasOne('User', 'GEN_ID_Usuario');
+	}
+
+	public function formularioTieneInformacionPersonal()
+    {
+        return ! is_null(
+            \DB::table('GEN_Info_Personal')
+              	->where('GEN_ID_Usuario', '=',$this->Usu_ID)
+              	->first()
+        );
+    }
 }
