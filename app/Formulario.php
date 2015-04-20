00c8465155ece90d0ddd26173771184959c9bef5
Isaac2709 <Isaac2709@hotmail.com>
2015-04-19 23:05:07 -0600

    Fix the relation between the 'User' model and 'Formulario' model

--- app/Formulario.php
+++ app/Formulario.php
@@ -15,17 +15,15 @@ class Formulario extends Model {
 	*/
 	public $timestamps = false;
 
-
-
 	// protected $fillable = [ 'IPe_ID', 'IPe_Nombre', 'IPe_Apellido', 'IPe_Genero', 'IPe_Pasaporte',
 	// 						'IPe_Fecha_Nac', 'IPe_ID_PaisRes', 'IPe_Telefono', 'IPe_Fax'];
 
 	public function aspirante(){
-		return $this->belongsTo('User', 'GEN_ID_Usuario');
+		return $this->belongsTo('App\User', 'Usu_ID');
 	}
 
 	public function informacion_aspirante(){
-		return $this->hasOne('User', 'GEN_ID_Usuario');
+		return $this->hasOne('InformacionAspirante', 'Asp_ID_InfoPer');
 	}
 
 	public function formularioTieneInformacionPersonal()
