77eda8761184b11969be67e827c8d6534e879e66
jeank23 <salas23111991@gmail.com>
2015-05-04 15:12:43 -0600

    4/5/2015_3:12pm_Formulario-listo-para-test

--- resources/views/formulario/index.blade.php
+++ resources/views/formulario/index.blade.php
@@ -187,6 +187,7 @@ hr.soften {
 				        <li><a data-toggle="tab" href="#conocimientoDeIdiomas">Conocimiento de Idiomas Distintos al Materno</a></li>
 				        <li><a data-toggle="tab" href="#accesoBibliotecas">Acceso a Bibliotecas / Prosesamiento de Datos</a></li>
 				        <li><a data-toggle="tab" href="#manejoDeProgramas">Manejo de Programas de Computación</a></li>
+				        <li><a data-toggle="tab" href="#recomendaciones">Recomendaciones</a></li>
 				    </ul>
 				    <div class="tab-content" id="myTabContent">
 				    	<!-- PERSONAL INFO -->
@@ -227,8 +228,13 @@ hr.soften {
 					        			<label for="id" class="col-md-4 control-label">Cedula o Pasaporte:</label>
 					        			<div class="col-md-8">
 					        				<input type="text" class="form-control" name="id" value="{{ $user->formulario->IPe_Pasaporte }}">
+					        				<div class="fileupload fileupload-new" data-provides="fileupload" >
+										    <span class="btn btn-default btn-file"><span class="fileupload-new">Buscar Archivo</span>
+										    <span class="fileupload-exists">Cambiar</span><input type="file" id="archivoCedulaInfPersonal" name="archivoCedulaInfPersonal" /></span>
+										    <span class="fileupload-preview"></span>
+										    <a href="#" class="close fileupload-exists" data-dismiss="fileupload" style="float: none">×</a>
+									  	</div>
 					        			</div>
-					        			
 					        		</div>
 					        		<!-- Genero del(la) aspirante -->
 									<div class="form-group">
@@ -316,17 +322,19 @@ hr.soften {
 					        			<button id="agregarNuevoEmail" type="button" class="btn btn-primary btn-sm agregarQuitarNuevoMail" onClick="cambiarTextoDeBoton(this.id)">+</button>
 					        		</div>
 
-									<!-- Fax -->
+									
+								</div>
+								<!-- End col-lg-6 -->
+
+	                    		<div class="col-lg-6">
+	                    			<!-- Fax -->
 									<div class="form-group">
 										<label for="fax" class="col-md-4 control-label">Fax:</label>
 										<div class="col-md-8">
 											<input type="text" class="form-control" name="fax" value="{{ $user->formulario->IPe_Fax }}">
 										</div>
 									</div>
-								</div>
-								<!-- End col-lg-6 -->
-
-	                    		<div class="col-lg-6">
+	                    			
 	                    			<!-- Enfasis de interes -->
 									<div class="form-group">
 										<label for="enfasis" class="col-md-4 control-label">Énfasis de interes:</label>
@@ -396,22 +404,12 @@ hr.soften {
 	                    		<br/>
 	                    		<!-- End col-lg-6 -->
 					        </div>
+					        <hr class="soften">
 					        <!-- End row -->
-					        <div class="row">
-					        	<div class="form-group">
-						        	<label for="direccion_correspondencia" class="col-md-10 control-label"></label>
-						        	<!-- name_control -->
-						        	<div class="col-md-2">
-						        		<div class="form-group">
-						        			<input type="submit" class="btn btn-success" value="Actualizar">
-						        		</div>
-						        	</div>
-		                    		<!-- <div class="col-md-6">
-					        			<button type="submit" class="btn btn-success btn-lg pull-right">Guardar</button>
-						        	</div> -->
-						        </div>
-					        </div>
-					        <!-- End row -->
+					        <div class="col-md-11">
+			    				<input id="btnActualizarDatosPersonales" class="btn btn-success btn-lg imagenSubmit" type="submit" value="&#xf0c7; Actualizar">
+							</div>
+							<br/>
 				        	</form>
 				        	<!-- End Form -->
 				        </div>
@@ -846,7 +844,7 @@ hr.soften {
 									<!--Archivo-->
 										<div class="form-group col-md-12 seleccionArchivoConocimientoDeIdiomas">
 											<!-- <label for="archivoConocimientoDeIdiomas" class="labelArchivoConocimientoDeIdiomas">Con relación al dominio del idioma inglés, adjunte documentos que certifiquen los cursos y programas estudiados, según el Marco Común Europeo de Referencia para las Lenguas (MCERL) o su equivalente certificado por una universidad o institución reconocida.</label>-->
-											<small>Con relación al dominio del idioma inglés, adjunte documentos que certifiquen los cursos y programas estudiados, según el <em> Marco Común Europeo de Referencia para las Lenguas</em> (MCERL) o su equivalente certificado por una universidad o institución reconocida.</small>
+											<p class="text-center"><small>Con relación al dominio del idioma inglés, adjunte documentos que certifiquen los cursos y programas estudiados, según el <em> Marco Común Europeo de Referencia para las Lenguas</em> (MCERL) o su equivalente certificado por una universidad o institución reconocida.</small></p>
 											<div class="fileupload fileupload-new " data-provides="fileupload">
 											    <span class="btn btn-default btn-file"><span class="fileupload-new">Buscar Archivo</span>
 											    <span class="fileupload-exists">Cambiar</span><input type="file" id="archivoConocimientoDeIdiomas" name="archivoConocimientoDeIdiomas" /></span>
@@ -1031,6 +1029,146 @@ hr.soften {
 								<br/>
 				    		</form>
 				    	</div>
+				    	<!-- Termina Manejo de Programas de Computacion-->
+
+				    	<!-- Recomendaciones-->
+				    	<div id="recomendaciones" class="tab-pane fade">
+				    		<form role="form" action="#" method="post" class="form-horizontal">
+				    			<input type="hidden" name="_token" value="{{ csrf_token() }}">	
+				    			<div id="formularioRecomendaciones1" class="row blockRecomendaciones">
+				    				<div class="row">
+				    					<div class="col-md-12">
+				    						<p class="text-center"><label>Profesores o Especialistas que lo (a) podrían recomendar a este programa</label></p>
+				    						<p class="text-center"><small>Indique el nombre y dirección de dos personas (uno de éstas debe ser un académico con nivel mínimo de Maestría) que lo (a) podrían recomendar a este  programa. Las recomendaciones deberán ser enviadas a nosotros,  por quien recomienda,  en sobre cerrado.</small></p>
+				    					</div>
+				    					<div class="col-md-6">
+				    						<!--Nombre-->
+						    				<div class="form-group">
+						    					<label for="nombre1" class="col-md-4 control-label labelNombre1">Nombre:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputNombre1" name="nombre1" id="nombre1" placeholder="Persona 1">
+						    					</div>
+						    				</div>
+						    				<!--Direccion-->
+						    				<div class="form-group">
+						    					<label for="direccion1" class="col-md-4 control-label labelDireccion1">Dirección:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputDireccion1" name="direccion1" id="direccion1" placeholder="Persona 1">
+						    					</div>
+						    				</div>
+						    				<!--Telefono-->
+						    				<div class="form-group">
+						    					<label for="telefono1" class="col-md-4 control-label labelTelefono1">Teléfono:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputTelefono1" name="telefono1" id="telefono1" placeholder="Persona 1">
+						    					</div>
+						    				</div>
+
+						    				<!--Fax-->
+						    				<div class="form-group">
+						    					<label for="fax1" class="col-md-4 control-label labelFax1">Fax:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputFax1" name="fax1" id="fax1" placeholder="Persona 1">
+						    					</div>
+						    				</div>
+
+						    				<!--Email-->
+						    				<div class="form-group">
+						    					<label for="email1" class="col-md-4 control-label labelEmail1">Email:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputEmail1" name="email1" id="email1" placeholder="Persona 1">
+						    					</div>
+						    				</div>
+
+						    				<!--Posicion-->
+						    				<div class="form-group">
+						    					<label for="posicion1" class="col-md-4 control-label labelPosicion1">Posición:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputPosicion1" name="posicion1" id="posicion1" placeholder="Persona 1">
+						    					</div>
+						    				</div>
+
+						    				<!--Carta-->
+						    				<div class="form-group">
+						    					<label for="archivoRecomendaciones1" class="col-md-4 control-label labelArchivoRecomendaciones1">Carta de referencia:</label>
+							    				<div class="fileupload fileupload-new col-md-8" data-provides="fileupload" >
+												    <span class="btn btn-default btn-file"><span class="fileupload-new">Buscar Archivo</span>
+												    <span class="fileupload-exists">Cambiar</span><input type="file" id="archivoRecomendaciones1" name="archivoRecomendaciones1" /></span>
+												    <span class="fileupload-preview"></span>
+												    <a href="#" class="close fileupload-exists" data-dismiss="fileupload" style="float: none">×</a>
+											  	</div>
+										  	</div>
+				    					</div>
+				    					<!--Termina col-md-6 -->
+
+				    					<div class="col-md-6">
+				    						<!--Nombre-->
+						    				<div class="form-group">
+						    					<label for="nombre2" class="col-md-4 control-label labelNombre2">Nombre:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputNombre2" name="nombre2" id="nombre2" placeholder="Persona 2">
+						    					</div>
+						    				</div>
+						    				<!--Direccion-->
+						    				<div class="form-group">
+						    					<label for="direccion2" class="col-md-4 control-label labelDireccion2">Dirección:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputDireccion2" name="direccion2" id="direccion2" placeholder="Persona 2">
+						    					</div>
+						    				</div>
+						    				<!--Telefono-->
+						    				<div class="form-group">
+						    					<label for="telefono2" class="col-md-4 control-label labelTelefono2">Teléfono:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputTelefono2" name="telefono2" id="telefono2" placeholder="Persona 2">
+						    					</div>
+						    				</div>
+
+						    				<!--Fax-->
+						    				<div class="form-group">
+						    					<label for="fax2" class="col-md-4 control-label labelFax2">Fax:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputFax2" name="fax2" id="fax2" placeholder="Persona 2">
+						    					</div>
+						    				</div>
+
+						    				<!--Email-->
+						    				<div class="form-group">
+						    					<label for="email2" class="col-md-4 control-label labelEmail2">Email:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputEmail2" name="email2" id="email2" placeholder="Persona 2">
+						    					</div>
+						    				</div>
+
+						    				<!--Posicion-->
+						    				<div class="form-group">
+						    					<label for="posicion2" class="col-md-4 control-label labelPosicion2">Posición:</label>
+						    					<div class="col-md-8">
+						    						<input type="text" class="form-control inputPosicion2" name="posicion2" id="posicion2" placeholder="Persona 2">
+						    					</div>
+						    				</div>
+
+						    				<!--Carta-->
+						    				<div class="form-group">
+						    					<label for="archivoRecomendaciones2" class="col-md-4 control-label labelArchivoRecomendaciones2">Carta de referencia:</label>
+							    				<div class="fileupload fileupload-new col-md-8" data-provides="fileupload" >
+												    <span class="btn btn-default btn-file"><span class="fileupload-new">Buscar Archivo</span>
+												    <span class="fileupload-exists">Cambiar</span><input type="file" id="archivoRecomendaciones2" name="archivoRecomendaciones2" /></span>
+												    <span class="fileupload-preview"></span>
+												    <a href="#" class="close fileupload-exists" data-dismiss="fileupload" style="float: none">×</a>
+											  	</div>
+										  	</div>
+				    					</div>
+				    				</div>
+				    				<hr class="soften">
+				    			</div>
+				    			<div class="col-md-11">
+				    				<input id="btnActualizarRecomendaciones" class="btn btn-success btn-lg imagenSubmit" type="submit" value="&#xf0c7; Actualizar">
+								</div>
+								<br/>
+				    		</form>
+				    	</div>
+				    	<!-- Termina Recomendaciones-->
 				    </div>
 					<!-- End Tabs -->
 				</div>
