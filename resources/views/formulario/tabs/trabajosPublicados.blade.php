5c79bcf0045ff4b9acdbf7fc7205f1825b59ee11
Isaac <Isaac2709@hotmail.com>
2015-05-07 11:07:21 -0600

    Upate indentation

--- resources/views/formulario/tabs/trabajosPublicados.blade.php
+++ resources/views/formulario/tabs/trabajosPublicados.blade.php
@@ -57,61 +57,61 @@
 		<?php $count = 1; ?>
 		@foreach($user->formulario->informacion_aspirante->publicaciones_desc as $publicacion)
 			<div id="formularioTrabajosPublicados{{ $count }}" class="row blockTrabajosPublicados">
-			<div class="row">
-				<div class="col-md-6">
-					<input type="hidden" name="id_pub[]" class="id_pub">
-					<!--Titulo de Publicacion-->
-					<div class="form-group">
-						<label for="tituloP" class="col-md-4 control-label labelTituloP">Título de la publicación:</label>
-						<div class="col-md-8">
-							<input type="text" class="form-control inputTituloP" name="titulo_publicacion[]" id="tituloP" value="{{ $publicacion->Pub_Titulo }}">
+				<div class="row">
+					<div class="col-md-6">
+						<input type="hidden" name="id_pub[]" class="id_pub">
+						<!--Titulo de Publicacion-->
+						<div class="form-group">
+							<label for="tituloP" class="col-md-4 control-label labelTituloP">Título de la publicación:</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control inputTituloP" name="titulo_publicacion[]" id="tituloP" value="{{ $publicacion->Pub_Titulo }}">
+							</div>
 						</div>
-					</div>
 
-					<!--Titulo del medio de publicacion-->
-					<div class="form-group">
-						<label for="tituloMP" class="col-md-4 control-label labelTituloMP">Título del medio de publicación:</label>
-						<div class="col-md-8">
-							<input type="text" class="form-control inputTituloMP" name="titulo_medio_publicacion[]" id="tituloMP" value="{{ $publicacion->Pub_Medio }}">
-						</div>
-					</div>
-				</div>
-				<!--Termina col-md-6-->
-
-				<div class="col-md-6">
-					<!--Pais de publicacion -->
-					<div class="form-group">
-						<label for="pais" class="col-md-4 control-label labelPais">País de publicación:</label>
-						<div class="col-md-8">
-							@if(!is_null($publicacion->Pub_ID_Pais))
-								<input type="text" name="pais[]" id="pais" class="form-control typeahead tt-query inputPais" autocomplete="off" spellcheck="false" value="{{ $publicacion->pais->Pais_Nombre }}">
-							@else
-								<input type="text" name="pais[]" id="pais" class="form-control typeahead tt-query inputPais" autocomplete="off" spellcheck="false">
-							@endif
-						</div>
-					</div>
-
-					<!--Año-->
-					<div class="form-group">
-						<label for="año" class="col-md-4 control-label labelAño">Año:</label>
-						<div class="col-md-8 " id="añoT" >
-	    					<div class="input-group date año">
-	    						<input type="text"  class="form-control inputAño" name="annio[]" id="año" value="{{ $publicacion->Pub_Anio }}">
-	    						<span class="input-group-addon "><i class="glyphicon glyphicon-th"></i></span>
+						<!--Titulo del medio de publicacion-->
+						<div class="form-group">
+							<label for="tituloMP" class="col-md-4 control-label labelTituloMP">Título del medio de publicación:</label>
+							<div class="col-md-8">
+								<input type="text" class="form-control inputTituloMP" name="titulo_medio_publicacion[]" id="tituloMP" value="{{ $publicacion->Pub_Medio }}">
 							</div>
 						</div>
 					</div>
+					<!--Termina col-md-6-->
+
+					<div class="col-md-6">
+						<!--Pais de publicacion -->
+						<div class="form-group">
+							<label for="pais" class="col-md-4 control-label labelPais">País de publicación:</label>
+							<div class="col-md-8">
+								@if(!is_null($publicacion->Pub_ID_Pais))
+									<input type="text" name="pais[]" id="pais" class="form-control typeahead tt-query inputPais" autocomplete="off" spellcheck="false" value="{{ $publicacion->pais->Pais_Nombre }}">
+								@else
+									<input type="text" name="pais[]" id="pais" class="form-control typeahead tt-query inputPais" autocomplete="off" spellcheck="false">
+								@endif
+							</div>
+						</div>
+
+						<!--Año-->
+						<div class="form-group">
+							<label for="año" class="col-md-4 control-label labelAño">Año:</label>
+							<div class="col-md-8 " id="añoT" >
+		    					<div class="input-group date año">
+		    						<input type="text"  class="form-control inputAño" name="annio[]" id="año" value="{{ $publicacion->Pub_Anio }}">
+		    						<span class="input-group-addon "><i class="glyphicon glyphicon-th"></i></span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!--Termina col-md-6-->
 				</div>
-				<!--Termina col-md-6-->
+				<!-- Termina row -->
+				<div class="col-md-offset-11">
+					<input type="checkbox" name="checkBox1" class="claseCheckboxTrabajosPublicados" id="checkboxTrabajosPublicados1" style="display:none">
+				</div>
+				<hr class="soften">
 			</div>
-			<!-- Termina row -->
-			<div class="col-md-offset-11">
-				<input type="checkbox" name="checkBox1" class="claseCheckboxTrabajosPublicados" id="checkboxTrabajosPublicados1" style="display:none">
-			</div>
-			<hr class="soften">
-		</div>
-		<!-- Termina formularioTrabajosPublicados -->
-		<?php $count = $count + 1; ?>
+			<!-- Termina formularioTrabajosPublicados -->
+			<?php $count = $count + 1; ?>
 		@endforeach
 	@endif
 
