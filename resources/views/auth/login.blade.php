73a5bdf20834166225fe1bbab91a88efc83acdff
Isaac2709 <Isaac2709@hotmail.com>
2015-05-25 17:30:13 -0600

    Tranlate the view of login to spanish

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,7 +21,7 @@
 							<div class="panel-body">
 								@if (count($errors) > 0)
 									<div class="alert alert-danger">
-										<strong>Whoops!</strong> There were some problems with your input.<br><br>
+										<strong>Whoops!</strong> Hubieron algunos problemas con las entradas.<br><br>
 										<ul>
 											@foreach ($errors->all() as $error)
 												<li>{{ $error }}</li>
@@ -34,14 +34,14 @@
 									<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 									<div class="form-group">
-										<label class="col-md-4 control-label">E-Mail Address</label>
+										<label class="col-md-4 control-label">Correo Electrónico</label>
 										<div class="col-md-6">
 											<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 										</div>
 									</div>
 
 									<div class="form-group">
-										<label class="col-md-4 control-label">Password</label>
+										<label class="col-md-4 control-label">Contraseña</label>
 										<div class="col-md-6">
 											<input type="password" class="form-control" name="password">
 										</div>
@@ -51,7 +51,7 @@
 										<div class="col-md-6 col-md-offset-4">
 											<div class="checkbox">
 												<label>
-													<input type="checkbox" name="remember"> Remember Me
+													<input type="checkbox" name="remember"> Recuérdame
 												</label>
 											</div>
 										</div>
@@ -59,16 +59,16 @@
 
 									<div class="form-group">
 										<div class="col-md-6 col-md-offset-4">
-											<button type="submit" class="btn btn-primary">Login</button>
+											<button type="submit" class="btn btn-primary">Iniciar Sesión</button>
 
-											<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
+											<a class="btn btn-link" href="{{ url('/password/email') }}">¿Olvidó su Contraseña?</a>
 										</div>
 									</div>
 									<div class="form-group">
 										<div class="col-md-6 col-md-offset-4">
 											¿No tienes una cuenta?
 
-											<a class="btn btn-link" href="{{ url('/auth/register') }}">Registrate</a>
+											<a class="btn btn-link" href="{{ url('/auth/register') }}">Regístrate</a>
 										</div>
 									</div>
 
