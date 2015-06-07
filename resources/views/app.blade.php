14cb83120d754cad75981d95c453a9a56f676429
Isaac2709 <Isaac2709@hotmail.com>
2015-06-07 15:49:27 -0600

    Updated the main menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,7 +41,7 @@
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
 					<li><a href="{{ url('/admin') }}">Home</a></li>
-					<li><a href="{{ url('/formulario') }}">Fomulario</a></li>
+					<li><a href="{{ url('/admin') }}">Fomulario</a></li>
 					<li><a href="{{ url('/admin/users') }}">Administradores</a></li>
 				</ul>
 
@@ -53,6 +53,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->Usu_Nombre }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
+								<li><a href="{{ URL::route('admin.users.show', Auth::user()->Usu_ID) }}">Perfil</a></li>
 								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 							</ul>
 						</li>
